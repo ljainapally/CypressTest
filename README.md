b8376bb645be1d05198a1f8ab29fd0a4059d1379
Lakshmi <lakshmi@verisart.com>
2019-11-21 12:24:25 +0000

    Login test2

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # CypressTest
+
+## Overview
+​
+This repo contains the cypress test automation suite
+​
+### Install required packages
+​
+You can install cypress and the required packages with with
+`npm install cypress --save-dev`
+`npm install cypress-cucumber-preprocessor --save-dev`
+`npm install`
+​
+# CypressTest
+to run tests do:
+`npm run test`
+​
+* TODO support other tests with `npm run MyTestSuite etc`
--- cypress/integration/AutomationTests/Login/Login.js
+++ cypress/integration/AutomationTests/Login/Login.js
@@ -1,17 +1,34 @@
   import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
   Given(`I am on the verisart website`, () => {
-      cy.visit('https://samo.verisart.com/login');
+    cy.visit('https://samo.verisart.com')
+    cy.contains('Log In').click().should('be.visible');   
     });
+
     When(`I enter the username`, () => {
-      cy.get('Input[name="email"]').type("samo_test@yopmail.com");
+    cy.get('Input[name="email"]').type("test_post1@yopmail.com").should('have.value','test_post1@yopmail.com');
     });
+
     When(`I enter the password`, () => {
-      cy.get('Input[name="password"]').type("Password123");
+    cy.get('Input[name="password"]').type("Password123").should('have.value','Password123');
     });
+
     When(`I click on the login button`, () => {
-     cy.get('.Button-kDSBcD').click();
+    cy.get('.Button-kDSBcD').click().should('have.text','Login');
     });
+
     Then(`I am logged in to the platform`, () => {
-        cy.get('.CertificatesPage__Content-cKGgNA').should('be.visible');
+    cy.get('.CertificatesPage__Content-cKGgNA').should('be.visible');
+    });
+
+    When(`I enter the username as {string}`, (userName) => {
+    cy.get('Input[name="email"]').type(userName);
     });
   
+    When(`I enter the password as {string}`, (password) => {
+      cy.get('Input[name="password"]').type(password,{log:false});
+    });
+
+    Then(`I should see the error as {string}`, (error) => {
+        cy.get('.styles__ErrorMessage-dspvmm').should('have.text',error) 
+        });
+       
